c0f9180b504260012ecb260a5c070d3d7186e321
Zion-El <mchladelola@gmail.com>
2022-08-05 08:20:20 +0100

    updated

--- dashboard.js
+++ dashboard.js
@@ -5,7 +5,7 @@ const showBtn = document.querySelector('.open_sidebar');
 const hideBtn = document.querySelector('.cancelBtn');
 
 showBtn.addEventListener('click', function(){
-    sidebar.style.marginLeft = '0px'
+    sidebar.style.marginLeft = '-40px'
     // let margin_count = -430;
     // const timer  = setInterval(() => {
     //     sidebar.style.marginLeft = `${margin_count}px`;
