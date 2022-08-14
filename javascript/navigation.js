9e9147f768e337913722096dc33f4e386664a83f
Bitee-cd <calebduniya45@gmail.com>
2022-08-14 03:07:07 +0100

    navigation bar responsiveness

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -1,8 +1,33 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+var hamburger= document.querySelector(".icon");
+var navMenu=document.querySelector(".nav-menu");
+var nav = document.getElementById("myTopnav")
+var navLink = document.querySelectorAll(".nav_links");
+var navButtons = document.querySelectorAll(".nav_buttons");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach(n => n.addEventListener("click", changeActive));
+navButtons.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu(){
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+
+}
+
+function changeActive(){
+
+      document.querySelector('.link.active').removeClass('active');
+      var $parent = $(this).parent();
+      $parent.addClass('active');
+      e.preventDefault();
+}
+      var url = window.location.href;
+      console.log(url)
